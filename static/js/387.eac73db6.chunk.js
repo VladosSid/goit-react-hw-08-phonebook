d4c1bbce622289e824ec61a5470900b9fcd4f53f
VladosSid <104505584+VladosSid@users.noreply.github.com>
2023-01-08 08:45:25 +0000

    Deploying to gh-pages from @ VladosSid/goit-react-hw-08-phonebook@a0ebfdbbca7aa3a3c663423afd0ceb1ce09cd23b 🚀

---
+++ static/js/387.eac73db6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[387],{6387:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var n=r(184);var a=function(){return(0,n.jsx)("b",{children:"Contacts"})}}}]);
+//# sourceMappingURL=387.eac73db6.chunk.js.map
